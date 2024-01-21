356caafb696651059433df517a4fb8d0a6f79ae7
Yeeloman <yami.onlyme@gmail.com>
2024-01-21 17:37:49 +0100

    add todo cli app

---
+++ src/components/Socialmedia.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let socialUrl = "";
+  export let socialName = "";
+</script>
+<div class="group bg-zinc-700 text-center 
+  w-80 h-16 py-2 mx-12 rounded-xl 
+  flex justify-center items-center 
+  hover:bg-lime-500">
+  <a
+    href={socialUrl}
+    target="_blank"
+    class="text-gray-50 font-bold group-hover:text-black"
+  >{socialName}</a>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import Socialmedia from '$components/Socialmedia.svelte'
+  let src = '/avatar-jessica.jpeg';
+  let socialmedia = {
+    github: {
+      name: "Github",
+      url: "https://github.com/yeeloman"
+    },
+    linkedin: {
+      name: "Linkedin",
+      url: "https://github.com/yeeloman"
+    },
+    instagram: {
+      name: "Instagram",
+      url: "https://github.com/yeeloman"
+    },
+    facebook: {
+      name: "Facebook",
+      url: "https://github.com/yeeloman"
+    }
+  };
+</script>
+<div class="w-auto h-auto bg-zinc-800 rounded-xl flex justify-center items-center flex-col">
+  <img
+    {src}
+    alt="profile picture"
+    class="rounded-full w-20 h-20 my-4 mt-10"
+  >
+  <h1
+    class="font-bold text-2xl text-white"
+  >Jessica Randle</h1>
+  <p class="text-lime-500 font-semibold text-xs py-1">London, United Kingdom</p>
+  <p class="text-white py-5 text-sm">"Front end developerand avid reader."</p>
+  <div class="space-y-2">
+    {#each Object.values(socialmedia) as {name, url}}
+      <Socialmedia 
+      socialUrl={ url }
+      socialName={ name }
+    />  
+    {/each}
+    <div class="h-10"></div>
+  </div>
+</div>
